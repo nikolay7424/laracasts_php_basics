6dc7578b2289ee104cbba11ccd8f2a0b07c0e22f
nikolay7424 <nikolay7424@gmail.com>
2020-07-03 02:24:50 +0300

    laracasts php basics

---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+
+$router->define([
+    'index.php' => 'controllers/index.php',
+    'index.php/about' => 'controllers/about.php',
+    'index.php/about/culture' => 'controllers/about-culture.php',
+    'index.php/contact' => 'controllers/contact.php'
+]);
